68856ea67b616f526be632cad0a1fcc4c39e396e
Sardor Ulmasov <immortalaigs@gmail.com>
2022-04-04 17:38:10 +0300

    Created By Sardor

---
+++ js/basicInfo.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        setTimeout(() => {
+            loader.style.display = 'none'
+        }, 500)
+    }, 2000)
+    })
+
+// Navbar part
+const burger = document.getElementById('nav');
+const navLists = document.getElementById('nav__lists');
+
+burger.addEventListener('click', () => {
+    navLists.style.transition = 1000;
+    navLists.classList.toggle('toggled')
+})
+
+// for footer
+
+const footerDate = document.getElementById('date');
+const date = new Date();
+let forFooter = date.getFullYear();
+footerDate.innerHTML = forFooter;
---
+++ js/science.js
@@ -0,0 +1,36 @@
+// Fetch api
+const urlAll = `https://inshortsapi.vercel.app/news?category=${'science'}`;
+
+const fetchData = async () => {
+    const res = await fetch(urlAll);
+    const req = await res.json()
+    let datas = req.data
+    
+    const container = document.querySelector('.container');
+    datas.map((data) => {
+        container.insertAdjacentHTML('beforeend', `
+            <div class="content grid-item-1">
+                <img class="image" src="${data.imageUrl}" alt="Relative images">
+                <div class="img_info">
+                    <h3 id="title">Title:  
+                        <span class="title_child">${data.title}</span>
+                    </h3>
+                    <p class="content_img__paragraph">${data.content}   <br> <br>
+                        <span class="date">${data.date}</span>
+                    </p>
+
+                    <p class="author">Author: ${data.author} 
+                        <span class="time">Uploaded - ${data.time}</span>
+                    </p>
+                    <h5>
+                        Read More: <a href="${data.readMoreUrl}" target="_blank">HERE</a>
+                    </h5>
+                </div>
+             </div>
+
+        `)
+    })
+    
+}
+
+fetchData()
